deb0931c791b7984200ff99c1ab8beb402add940
Alvim1234 <lucasalvimetec@gmail.com>
2025-11-28 10:43:25 -0300

    Fiz alguns bagulho que eu não sei o nome

--- src/OculosSol.java
+++ src/OculosSol.java
@@ -1,4 +1,19 @@
 
-public class OculosSol {
+public class OculosSol extends Produtos{
 
+	String corLente;
+	
+	public OculosSol(String cor, float preco, String pigmentacao){
+		super(cor, preco);
+		
+		this.corLente = pigmentacao;
+	}
+	
+	public void setCorLente(String pigmento){
+		this.corLente = pigmento;
+	}
+	
+	public String getCorLente(){
+		return corLente;
+	}
 }
